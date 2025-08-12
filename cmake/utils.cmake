9cecbd5bd36ecf5205a2b8308b9185832340b0d4
Menachem Adin <meniadin@gmail.com>
2025-08-12 17:32:31 +0300

    Files alignment

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -2,7 +2,7 @@ function(${PROJECT_NAME}_print_variable variable)
     if(DEFINED ${variable})
         message(STATUS "${variable} = ${${variable}}")
     else()
-        message(FATAL_ERROR "Variable '${variable}' is not defined.")
+        message(STATUS "Variable '${variable}' is not defined.")
     endif()
 endfunction()
 
@@ -14,7 +14,6 @@ function(${PROJECT_NAME}_set_compiler_flags)
         endif()
     elseif(CMAKE_C_COMPILER_ID STREQUAL "MSVC")
         add_compile_options(
-            /EHsc
             /wd5072
         )
     else()
@@ -90,6 +89,7 @@ function(${PROJECT_NAME}_set_target_cpp_compiler_flags target)
         endif()
     elseif(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
         target_compile_options(${target} PRIVATE
+            /EHsc
             /Wall
             /WX
             /wd4514
@@ -100,6 +100,7 @@ function(${PROJECT_NAME}_set_target_cpp_compiler_flags target)
             /wd4820
             /wd5026
             /wd5027
+            /wd5045
             /wd5072
         )
     else()
